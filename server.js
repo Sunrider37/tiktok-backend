be3b730b9cb0c31786293949f02079e5d760680f
Sunrider37 <Sunriderr37@gmail.com>
2021-06-12 19:48:52 +0300

    sd

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import Videos from './dbModel.js'
 
 
 const app = express();
-const port = process.env.PORT | 8080;
+const port = process.env.PORT | 9000;
 
 app.use(express.json());
 app.use((req, res, next) => {
