dcb384ab311c98fe3cc91a7cf1731e14f08780a6
Sunrider37 <Sunriderr37@gmail.com>
2021-06-12 18:18:27 +0300

    make it better

---
+++ data.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+        url : "https://www.tiktok.com/@officialfluff4/video/6955559006563863813",
+        channel : "safagags",
+        song : "Waste my hate on you",
+        likes: "3213214",
+        messages: "434325",
+        description: "dsadsad",
+        shares: "500"
+
+},
+{
+
+}]
+
+export default data;
